7af524b20b7a9a5cfc0951c76d4502ddd38ebc02
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-09-20 23:04:06 -0500

    Updated comments

--- Sorting Algorithms/QuickSort.cpp
+++ Sorting Algorithms/QuickSort.cpp
@@ -9,7 +9,7 @@ int partition(int arr[], int low, int high)
     int pivot = arr[high]; // pivot
     int i = (low - 1);     // Index of smaller element
 
-    for (int j = low; j <= high - 1; j++)
+    for (int j = low; j <= high - 1; j++) // if current element smaller than or equal to the pivot
     {
         // If current element is smaller than or equal to pivot
         if (arr[j] <= pivot)
@@ -18,21 +18,21 @@ int partition(int arr[], int low, int high)
             swap(arr[i], arr[j]); // swap elements into correct position
         }
     }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
+    swap(arr[i + 1], arr[high]); // swap pivot into correct position
+    return (i + 1);              // return pivot index
 }
 
 // write a quicksort function
-void quickSort(int arr[], int low, int high)
+void quickSort(int arr[], int low, int high) // low is 1st index and high is the last index
 {
     if (low < high)
     {
         // pi is partitioning index, arr[p] is now at right place
-        int pi = partition(arr, low, high);
+        int newPivot = partition(arr, low, high);
 
         // separately sort elements before partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        quickSort(arr, low, newPivot - 1);
+        quickSort(arr, newPivot + 1, high);
     }
 }
 
@@ -48,7 +48,7 @@ void printArray(int arr[], int n)
 int main()
 {
     int arr[] = {5, 13, 2, 25, 7, 17, 20, 8, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]); // get the size of the array
     quickSort(arr, 0, n - 1);
     cout << "Sorted array: \n";
     printArray(arr, n);
